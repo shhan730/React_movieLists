080fca138b1388403791c0dcd76c4c256fa1f0a9
SeungHun Han <shhan730@gmail.com>
2019-12-30 21:18:30 +0900

    Set Move Component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { Item } from 'semantic-ui-react'
 
 function Movie({id, year, title, summary, poster, genres}){
+  const items = {
+    childKey: id,
+    image: poster,
+    header: title,
+    description: summary,
+    meta: year,
+    extra: genres,
+  }
+
   return (
-    <div className="movie">
-      <img src={poster} alt={title} title={title}/>
+    <div>
+      <Item items={items}/>
     </div>
   );
+
 }
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default Movie;
+
