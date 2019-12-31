5d1a78b70ebb22ac90704d83142964c55db95469
SeungHun Han <shhan730@gmail.com>
2019-12-31 22:03:48 +0900

    Add Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Home from './routes/Home'
+import { HashRouter, Route} from "react-router-dom";
 
 function App() {
-  return <div></div>
+  return (
+    <HashRouter>
+      <Route path='/' component={Home}></Route>
+    </HashRouter>
+  );
 }
 
 export default App;
